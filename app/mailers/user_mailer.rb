7fc5091d12d74eb7f075b82175e47f870471e76c
Bob Cook <bobcook@sunrem.com>
2014-12-31 21:18:52 -0700

    assets precomplie for Heroku

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,8 @@
 class UserMailer < ActionMailer::Base
-  default from: "Do_not_reply@storyitr8.com"
+  default from: "do-not-reply@storyitr8.com"
 
   def contact_mailer(contact)
     @contact = contact
-    mail(to; Rails.application.secrets.owner_email, from: @contact.email, :subject => "Website Contact")
+    mail(to: Rails.application.secrets.owner_email, from: @contact.email, :subject => "Website Contact")
   end
 end
